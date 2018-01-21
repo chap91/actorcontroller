518ba1413817cc7323e4f01d3ccc634205124ae4
Florian Pawlik <pawlik@fbihome.de>
2018-01-21 23:06:15 +0100

    implementet first funktions and added logging (spdlog)

---
+++ actor.cpp
@@ -0,0 +1,40 @@
+#include "actor.h"
+
+Actor::Actor(){    
+    _logger = spdlogger::basic_logger_st("ActorLogging", "./log/actor.log");
+}
+
+Actor::~Actor(){
+    for(auto element : _actions) {
+        delete element;
+    }
+}
+
+Action Actor::getAction(std::string name) {
+    return null;
+}
+
+void Actor::setAction(Action action) {
+    _actions.put(action);
+}
+
+std::string Actor::getName(){
+    return this->_name;
+}
+
+void Actor::send(Action action) const;{
+       
+    try {
+        
+        if (action.send != nullptr){
+            action.send(action._sendData);
+            _logger->info("action complete");
+        
+        } else {
+            throw std::exception(this->_name + "the action:\"" + action._name + "\"action got no sendFunction");
+        }
+
+    } catch (std::exception& e) {
+        _logger->error(e.what());
+    }
+}
--- actor.h
+++ actor.h
@@ -1,28 +1,54 @@
-#include <string.h>
-#include <iostream>
+#include <string>
 #include <vector>
 #include <functional>
+#include <shared_ptr>
+#include "./spdlog/spdlog.h"
 
-template<class T>
-class Action {
-public:
-    std::function<std::bool (T)> send;
-    
-private:
-    std::string _name;
-    std::T _sendData;
-};
 
 // [x](int n) {return n < x;}
 // function<int (int)> func = [](int i) {return i + 4};
 
-class Actor {
+/**
+ * Eine Aktion ist steuert einen Aktor
+ * z.B. turnOn, turnOff, reset oder getInfo
+ * 
+ * template T: Typ der Variable, welche verschickt werden soll 
+ * z.B. Integer, String, Binary
+ */
+template<class T> class Action {
 public:
-    Action getAction(std::string);
-    void setAction(Action);
+    /**
+     * Lampda-Funktion zum verschicken der einer Aktion
+     * so kann ein Aktor mehrere Typen der Ansteuerung haben
+     * z.B. über das Wlan, einer RestApi oder über eine Funkanbindung
+     */ 
+    std::function<void (T)> send;
 
 private:
     std::string _name;
-    std::vector<Action> _actions;
+    std::T _sendData;
+    spdlog::basic_logger_mt("basic_logger", "logs/log.txt") _logger;
 };
 
+
+/**
+ * Ein Aktor ist eine Steuereinheit
+ * z.B. eine Funk- oder Wlan-Steckdose 
+ * 
+ * Ein Aktor verfügt über mehrere Aktionen welche den Aktor steuern
+ */
+class Actor {
+public:
+    Actor();
+    ~Actor();
+
+    std::string getName();
+    Action getAction(std::string);
+    void setAction(Action);
+    void send(Action);
+
+private:
+    std::string _name;
+    std::vector<Action*> _actions;
+    std::shared_ptr<logger> _logger;
+};
